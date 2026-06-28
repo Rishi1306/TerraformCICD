1daf3d7348501307bfc760e4042347af225dbc59
Rishi Kejriwal <rishikejriwalrk@gmail.com>
2026-06-28 18:13:00 +0530

    basic updates

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "pipe-rg-2" {
   name = "rike-pipe-2-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "pipe-rg-3" {
+  name = "rike-pipe-3-rg"
+  location = "centralindia"
+}
