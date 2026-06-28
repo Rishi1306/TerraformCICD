17c5a6d9f3e640cf607c6eb27931583f197ed67a
Rishi Kejriwal <rishikejriwalrk@gmail.com>
2026-06-28 19:07:33 +0530

    rg4 added

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "pipe-rg-3" {
   name = "rike-pipe-3-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "pipe-rg-4" {
+  name = "rike-pipe-4-rg"
+  location = "centralindia"
+}
