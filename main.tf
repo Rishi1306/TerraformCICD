e3c155f0b6bb920c9277aa71622fcaa95af76467
Rishi Kejriwal <rishikejriwalrk@gmail.com>
2026-06-28 13:47:12 +0530

    Resource 5 added and pipeline update

--- main.tf
+++ main.tf
@@ -17,3 +17,8 @@ resource "azurerm_resource_group" "pipe-rg-4" {
   name = "rike-pipe-4-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "pipe-rg-5" {
+  name = "rike-pipe-5-rg"
+  location = "centralindia"
+}
